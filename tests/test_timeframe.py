a4454564471d176d6fe38abb02c41b51e70b7c85
Shane Smith <shane.smith@altec.com>
2021-02-01 22:56:01 -0600

    Started creating unit test for timeframe, realized I need to change it.

--- tests/test_timeframe.py
+++ tests/test_timeframe.py
@@ -1,3 +1,12 @@
 """
 Test for timeframe.py
 """
+
+from jira_reports.timeframe import report_dates
+
+birth_date = "1974-08-14"
+freeze_date = "1999-12-31"
+start_of_year = "2021-01-01"
+
+def test_report_dates():
+    pass
